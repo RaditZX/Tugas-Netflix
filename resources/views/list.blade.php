f8f998d7c8eac5884ac3fbba4d71255582f78fa2
RaditZX <daivaraditya36@gmail.com>
2022-03-24 19:58:32 +0700

    perbaikan3

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -11,7 +11,7 @@
           <h1 id="home"></h1>
     
        
-          <h1>Popular anime</h1>
+          <h1 style="font-weight:bold; font-size:50px;">Popular anime</h1>
           <div class="box">
           @foreach ($films as $film)
          
@@ -28,7 +28,7 @@
       </div>
       
 
-      <h1 id="myList">Trending Anime</h1>
+      <h1 id="myList" style="font-weight:bold; font-size:50px;">Trending Anime</h1>
       <div class="box">
       
       @foreach ($films->shuffle() as $film)
@@ -37,22 +37,22 @@
                       
       </div>
       
-      <h1 id="tvShows">TV Shows</h1>
-      <div class="box">
+      <h1 id="tvShows" style="font-weight:bold; font-size:50px;">TV Shows</h1>
+      <div class="box" style="font-weight:bold; font-size:50px;">
       @foreach ($films->shuffle() as $film)
       <a href="/detail/{{ ($film->id) }}"><img src="{{asset('storage/'.$film->image)}}" alt=""></a>
             @endforeach             
       </div>
       
 
-      <h1 id="movies">Blockbuster Action & Adventure</h1>
+      <h1 id="movies" style="font-weight:bold; font-size:50px;">Blockbuster Action & Adventure</h1>
       <div class="box">
       @foreach ($films->shuffle() as $film)
       <a href="/detail/{{ ($film->id) }}"><img src="{{asset('storage/'.$film->image)}}" alt=""></a>
             @endforeach  
       </div>
 
-      <h1 id="originals">Netflix Originals</h1>
+      <h1 id="originals" style="font-weight:bold; font-size:50px;">Netflix Originals</h1>
       <div class="box">
       @foreach ($films->shuffle() as $film)
       <a href="/detail/{{ ($film->id) }}"><img src="{{asset('storage/'.$film->image)}}" alt=""></a>
